0643305e620823f13936d0e50091a504aa055525
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-07-23 17:41:09 +0530

    Initial commit

--- src/Components/AccountCircle.js
+++ src/Components/AccountCircle.js
@@ -13,141 +13,143 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 
-
 const AccountCircle = () => {
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(0);
-  const { theme } = useTheme();
 
-  const navigate = useNavigate();
-  const [user] = useAuthState(auth);
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(0);
+    const { theme } = useTheme();
 
-  const handleModalOpen = () => {
-    if (user) {
-      // If user is already logged in, navigate to the user page
-      navigate('/user');
-    } else {
-      setOpen(true);
+    const navigate = useNavigate();
+    const [user] = useAuthState(auth);
+
+
+    const handleModalOpen = () => {
+
+        if (user) {
+            //navigate to the userpage
+            navigate('/user');
+        }
+        else {
+            setOpen(true);
+        }
     }
-  };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    }
 
-  const handleValueChange = (e, v) => {
-    setValue(v);
-  };
+    const handleValueChange = (e, v) => {
+        setValue(v);
+    }
 
-  const logout = () => {
-    auth.signOut()
-      .then(res => {
-        toast.success('Logged out', {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-        });
-        navigate('/');
-      })
-      .catch(err => {
-        toast.error('Not able to log out', {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-        });
-      });
-  };
+    const logout = () => {
+        auth.signOut().then(res => {
+            toast.success('Logged out', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+            navigate('/')
+        }).catch(err => {
+            toast.error('Not able to log out', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+        })
+    }
 
-  const googleProvider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
 
-  const handleGoogleSignIn = () => {
-    signInWithPopup(auth, googleProvider)
-      .then((res) => {
-        toast.success('Google login successful', {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-        });
-        handleClose();
-      })
-      .catch(err => {
-        toast.err(errorMapping[err.code] || 'Not able to use Google authentication', {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-        });
-      });
-  };
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, googleProvider).then((res) => {
+            toast.success('Google login successful', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+            handleClose();
+        }).catch(err => {
+            toast.err(errorMapping[err.code] || 'Not able to use google authentication', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+        })
+    }
 
-  return (
-    <div>
-      <span className="Tooltip">
-        <AccountCircleIcon style={{cursor:'pointer'}} onClick={handleModalOpen} />
-      </span>
-      {user && (
-        <span className="ToolTip">
-          <LogoutIcon style={{cursor:'pointer'}} onClick={logout} />
-        </span>
-      )}
+    return (
+        <div>
+            <span className="Tooltip">
+                <AccountCircleIcon style={{cursor:'pointer'}} onClick={handleModalOpen} />
+            </span>
+            {
+                user &&
+                <span className="ToolTip">
+                    <LogoutIcon style={{cursor:'pointer'}} onClick={logout} />
+                </span>
+            }
 
-      <Modal
-        open={open}
-        onClose={handleClose}
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <div style={{ width: '400px', textAlign: 'center' }}>
-          <AppBar position='static' style={{ background: 'transparent' }}>
-            <Tabs
-              value={value}
-              onChange={handleValueChange}
-              variant='fullWidth'
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
             >
-              <Tab label='Login' style={{ color: theme.textColor }}></Tab>
-              <Tab label='Signup' style={{ color: theme.textColor }}></Tab>
-            </Tabs>
-          </AppBar>
+                <div style={{ width: '400px', textAlign: 'center' }}>
+                    <AppBar position='static' style={{ background: 'transparent' }}>
+                        <Tabs
+                            value={value}
+                            onChange={handleValueChange}
+                            variant='fullWidth'
+                        >
+                            <Tab label='login' style={{ color: theme.textColor }} ></Tab>
+                            <Tab label='signup' style={{ color: theme.textColor }}></Tab>
+                        </Tabs>
+                    </AppBar>
 
-          {value === 0 && <LoginForm handleClose={handleClose} />}
-          {value === 1 && <SignUpForm handleClose={handleClose} />}
+                    {value === 0 && <LoginForm handleClose={handleClose} />}
+                    {value === 1 && <SignUpForm handleClose={handleClose} />}
 
-          <Box>
-            <span>Or</span>
-            <GoogleButton
-              style={{
-                width: '100%',
-                marginTop: '10px'
-              }}
-              onClick={handleGoogleSignIn}
-            />
-          </Box>
+
+                    <Box>
+                        <span>Or</span>
+                        <GoogleButton
+                            style={{
+                                width: '100%',
+                                marginTop: '10px'
+                            }}
+                            onClick={handleGoogleSignIn}
+                        />
+                    </Box>
+                </div>
+            </Modal>
         </div>
-      </Modal>
-    </div>
-  );
+    )
 }
 
-export default AccountCircle;
+export default AccountCircle
